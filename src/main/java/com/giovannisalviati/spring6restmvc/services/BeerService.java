cc19e55a58dc77c02fed90a6f891062955bcc0c4
G-Salviati <giovannisalviati@gmail.com>
2024-07-07 00:15:48 +0200

    Edited service and controller to return a list of beers in restful way (JSON)

--- src/main/java/com/giovannisalviati/spring6restmvc/services/BeerService.java
+++ src/main/java/com/giovannisalviati/spring6restmvc/services/BeerService.java
@@ -2,8 +2,11 @@ package com.giovannisalviati.spring6restmvc.services;
 
 import com.giovannisalviati.spring6restmvc.models.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+    List<Beer> listBeer();
+
     Beer getBeerById(UUID id);
 }
